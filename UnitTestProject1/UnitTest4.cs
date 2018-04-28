8696e414bdae3732076074315e976ec88e10b7f7
NikitaSpes <nikitos-man001@yandex.ru>
2018-04-28 22:20:19 +0700

    add test 9(2)

--- UnitTestProject1/UnitTest4.cs
+++ UnitTestProject1/UnitTest4.cs
@@ -16,7 +16,7 @@ using System.Linq;
 namespace TestClick
 {
     [TestClass]
-    public class UnitTest3
+    public class UnitTest4
     {
         static IWebDriver Browser;
         static WebDriverWait wait;
@@ -81,60 +81,116 @@ namespace TestClick
         }
 
         [Test]
-        public void TestMethod3()
+        public void TestMethod4()
         {
-            Browser.Navigate().GoToUrl("http://localhost/litecart/admin/?app=countries&doc=countries");
+            Browser.Navigate().GoToUrl("http://localhost/litecart/en/");
 
-            if ((IsElementExists(By.Name("username")) == true) && (IsElementExists(By.Name("password")) == true))
-            {
-                IWebElement LohinInput = Browser.FindElement(By.Name("username"));
-                IWebElement PasswordInput = Browser.FindElement(By.Name("password"));
-                LohinInput.SendKeys("admin");
-                PasswordInput.SendKeys("admin" + OpenQA.Selenium.Keys.Enter);
-            }
-            else
-            {
-                throw new ArgumentException("Нет полей для ввода");
-            }
+            int i;
+            string s;
 
+            string header1;
+            string header2;
+
+            string price1;
+            string price2;
+
+            string price3;
+            string price4;
+
+
+            List<IWebElement> popular = Browser.FindElements(By.CssSelector(".listing-wrapper > li")).ToList();
+            for (i = 0; i < popular.Count; i++)
+            {
+                IWebElement num1 = Browser.FindElements(By.CssSelector(".listing-wrapper > li"))[i];                
+
+                if (AreElementsPresent(num1, By.CssSelector("a.link")) == true)
+                {
+                    IWebElement main_link = num1.FindElement(By.CssSelector("a.link"));
+                    IWebElement product_name1 = num1.FindElement(By.CssSelector("a.link > div.name"));
+                    s = (main_link.GetAttribute("href"));
+                    header1 = product_name1.GetAttribute("textContent");
+
+                    if (AreElementsPresent(num1, By.CssSelector("a.link> div.price-wrapper > span")) == true)
+                    {
+                        IWebElement product_price1 = num1.FindElement(By.CssSelector("a.link> div.price-wrapper > span"));
+                        price1 = product_name1.GetAttribute("textContent");
+                    }
+                    else if (AreElementsPresent(num1, By.CssSelector("a.link> div.price-wrapper > s")) == true)
+                    {
+                        IWebElement product_price3 = num1.FindElement(By.CssSelector("a.link> div.price-wrapper > s"));
+                        price3 = product_price3.GetAttribute("textContent");
+                    }
+
+                    openInNewWindow((main_link.GetAttribute("href")));
+                    Browser.SwitchTo().Window(Browser.WindowHandles[1]);
+
+                    IWebElement product_name2 = Browser.FindElement(By.CssSelector("#box-product > div > h1"));                  
+                    header2 = product_name2.GetAttribute("textContent");
+                    if (header1 != header2)
+                    {
+                        throw new ArgumentException("разные заголовки");
+                    }
+
+                   /* IWebElement product_name2 = Browser.FindElement(By.CssSelector("#box-product > div > h1"));
+                    if (AreElementsPresent(num1, By.CssSelector("#box-product > div.content > div.information > div.price-wrapper > span")) == true)
+                    {
+                        IWebElement product_price2 = Browser.FindElement(By.CssSelector("#box-product > div.content > div.information > div.price-wrapper > span"));
+                        price2 = product_name1.GetAttribute("textContent");
+                    }
+                    else if (AreElementsPresent(num1, By.CssSelector("a.link> div.price-wrapper > s")) == true)
+                    {
+                        IWebElement product_price4 = num1.FindElement(By.CssSelector("a.link> div.price-wrapper > s"));
+                        price4 = product_price4.GetAttribute("textContent");
+                    }*/
+                   
+
+
+
+
+                    Browser.Close();
+                    Browser.SwitchTo().Window(Browser.WindowHandles[0]);
+                }
+                else
+                {
+                    throw new ArgumentException("Нет елемента");
+                }
+            }
+            
+/*
             int i;
             int j;
 
             int key1 = 0;
             int key2 = 0;
 
-            int key3 = 0;
-
             int alfavit1;
             int alfavit2;
-
-            int alfavit3;
 
             string n;
             string m;
 
 
             List<IWebElement> Countries = Browser.FindElements(By.CssSelector("#content > form > table > tbody > tr.row")).ToList();
-              string[] dd = new string[Countries.Count];
-              for (i = 0; i < Countries.Count; i++)
-              {
+            string[] dd = new string[Countries.Count];
+            for (i = 0; i < Countries.Count; i++)
+            {
                 IWebElement num1 = Browser.FindElements(By.CssSelector("#content > form > table > tbody > tr.row"))[i];
                 IWebElement lll = num1.FindElement(By.CssSelector("td:nth-child(5) > a"));
                 IWebElement mmm = num1.FindElement(By.CssSelector("td:nth-child(6)"));
                 n = mmm.GetAttribute("textContent");
-            //    string ner = "some_test";
+                //    string ner = "some_test";
                 if (n != "0")
-                {                   
+                {
                     openInNewWindow((lll.GetAttribute("href")));
 
-                   // String countr = FindWindow((lll.GetAttribute("href")));
-                   // Browser.SwitchTo().Window(countr);
+                    // String countr = FindWindow((lll.GetAttribute("href")));
+                    // Browser.SwitchTo().Window(countr);
 
 
                     Browser.SwitchTo().Window(Browser.WindowHandles[1]);
                     List<IWebElement> Zones = Browser.FindElements(By.CssSelector("#table-zones > tbody > tr:not(.header)")).ToList();
-                    string[] nn = new string[(Zones.Count)-1];
-                    for (j = 0; j < (Zones.Count)-1; j++)
+                    string[] nn = new string[(Zones.Count) - 1];
+                    for (j = 0; j < (Zones.Count) - 1; j++)
                     {
                         IWebElement num2 = Browser.FindElements(By.CssSelector("#table-zones > tbody > tr:not(.header)"))[j];
                         IWebElement sss = num2.FindElement(By.CssSelector("td:nth-child(3)"));
@@ -155,60 +211,25 @@ namespace TestClick
                     }
 
                     key2 = 0;
-                   
+
                     Browser.Close();
-                }               
-                            
+                }
+
                 Browser.SwitchTo().Window(Browser.WindowHandles[0]);
 
                 dd[i] = lll.GetAttribute("textContent");
                 key1++;
                 if (key1 > 1)
                 {
-                    alfavit1 = String.Compare(dd[i-1], dd[i]);
+                    alfavit1 = String.Compare(dd[i - 1], dd[i]);
 
                     if (alfavit1 > 0)
                     {
                         throw new ArgumentException("Страны не по алфавиту");
                     }
                 }
-            }
+            }*/
 
-            Browser.Navigate().GoToUrl("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");
-            List<IWebElement> Zones_Countr_S = Browser.FindElements(By.CssSelector("#content > form > table > tbody > tr.row")).ToList();
-            for (i = 0; i < Zones_Countr_S.Count; i++)
-            {
-                IWebElement Zones_Countr = Browser.FindElements(By.CssSelector("#content > form > table > tbody > tr.row"))[i];
-                IWebElement Zone_link = Zones_Countr.FindElement(By.CssSelector("td:nth-child(3) > a"));
-                openInNewWindow((Zone_link.GetAttribute("href")));
-                Browser.SwitchTo().Window(Browser.WindowHandles[1]);
-
-                List<IWebElement> Zones_s = Browser.FindElements(By.CssSelector("#table-zones > tbody > tr:not(.header)")).ToList();
-                string[] sss = new string[(Zones_s.Count) - 1];
-                for (j = 0; j < Zones_s.Count-1; j++)
-                {
-                    IWebElement Zones_ss = Browser.FindElements(By.CssSelector("#table-zones > tbody > tr:not(.header)"))[j];
-                    IWebElement eee = Zones_ss.FindElement(By.CssSelector("td:nth-child(3) > select > option[selected=selected]"));
-                    m = eee.GetAttribute("textContent");                  
-                    key3++;
-
-                    if (key3 > 1)
-                    {
-                        alfavit3 = String.Compare(sss[j - 1], sss[j]);
-
-                        if (alfavit3 > 0)
-                        {
-                            throw new ArgumentException("Страны не по алфавиту");
-                        }
-                    }
-
-
-                }
-                key3 = 0;
-                Browser.Close();
-                Browser.SwitchTo().Window(Browser.WindowHandles[0]);
-
-            }
 
         }
 
